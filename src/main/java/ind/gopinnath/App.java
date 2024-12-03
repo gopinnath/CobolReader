78db16bf49b97527e4f1a02a4b8cbf5de309939f
Gopinath Radhakrishnan <gopinnath@gmail.com>
2024-12-03 20:36:59 +0530

    Sample Main Code

--- src/main/java/ind/gopinnath/App.java
+++ src/main/java/ind/gopinnath/App.java
@@ -1,13 +1,21 @@
 package ind.gopinnath;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import java.io.IOException;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws IOException {
+        CharStream charStream = CharStreams.fromFileName("./src/main/cobol/welcome.cobol");
+        Cobol85Lexer lexer = new Cobol85Lexer(charStream);
+        CommonTokenStream tokenStream = new CommonTokenStream(lexer);
+        Cobol85Parser parser = new Cobol85Parser(tokenStream);
+        ParseTree parseTree = parser.compilationUnit();
+
+        System.out.println(parseTree);
     }
 }
